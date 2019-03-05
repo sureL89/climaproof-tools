9f8fea006c12574b82f5e85d1df0374ff5a0b6e0
Georg Seyerl <georg.seyerl@gmail.com>
2019-03-05 12:07:46 +0100

    Add description and rudimentary error message

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from bokeh.palettes import Spectral6
 from cmst import Cmst
 import numpy as np
 import json
+from os.path import dirname, join
+
 
 # Workaround to show a spinner while loading ---------------------
 spinner_text = """
@@ -47,38 +49,67 @@ def get_dataset():
     cmst_upd = Cmst(bounding_box, sel_time_mean=inp_time_mean.value,
                    sel_experiment = inp_exp.value, work_dir='../data')
 
-    pdf_ts_upd = {}
-    for k in cmst_upd.time_selector:
-        pdf_ts_upd[k] = bmo.ColumnDataSource(data=cmst_upd.get_pandas_df(k))
+    return cmst_upd
 
-    return bmo.ColumnDataSource(data=cmst_upd.get_pandas_df('near'))
+def gen_upd_plot(event):
+    try:
+        if inp_gen_upd.label != "Update":
+            cmst_init = get_dataset()
 
-def update_plot(event):
-    # src = get_dataset()
-    # pdf_ts['near'].data.update(src.data)
-    # hide_spinner()
-    bounding_box = {'lat': np.array(json.loads(inp_lat.value)),
-                    'lon': np.array(json.loads(inp_lon.value))}
+            for k in cmst_init.time_selector:
+                pdf_ts[k] = bmo.ColumnDataSource(data=cmst_init.get_pandas_df(k))
 
-    cmst_upd = Cmst(bounding_box, sel_time_mean=inp_time_mean.value,
-                   sel_experiment = inp_exp.value, work_dir='../data')
+                # Create plot
+                p = bpl.figure(tools=[TOOLS,hover],toolbar_location="right")
+                p.circle(x='tasmax', y='pr',
+                    source = pdf_ts[k],
+                    size=12)
 
-    pdf_ts_upd = {}
-    for k in cmst_upd.time_selector:
-        pdf_ts_upd = bmo.ColumnDataSource(data=cmst_upd.get_pandas_df(k))
-        pdf_ts[k].data.update(pdf_ts_upd.data)
+                p.xaxis.axis_label = "climate change signal tasmax"
+                p.yaxis.axis_label = "climate change signal pr"
 
-    hide_spinner()
+                # Horizontal line
+                hline = bmo.Span(location=0, dimension='width', line_color='black', line_width=3)
+                p.renderers.extend([hline])
+
+                # Create table
+                data_table = bmo.widgets.DataTable(source=pdf_ts[k], columns=columns, fit_columns=False,
+                                                selectable='checkbox', height=p.plot_height, index_position=None)
+
+                l_panel = bo.layouts.row([p, data_table])
+
+                ls_tab.append(bmo.widgets.Panel(child=l_panel, title=k))
+
+            tabs = bmo.widgets.Tabs(tabs=ls_tab)
+            l.children[-1] = tabs
+            inp_gen_upd.label = "Update"
+
+        else:
+            cmst_upd = get_dataset()
+            pdf_ts_upd = {}
+            for k in cmst_upd.time_selector:
+                pdf_ts_upd = bmo.ColumnDataSource(data=cmst_upd.get_pandas_df(k))
+                pdf_ts[k].data.update(pdf_ts_upd.data)
+
+        hide_spinner()
+    except:
+        div_spinner.text="""
+        <div align="left" text-align="left">
+        <h2>Error!</h2>
+        <p text-align="left">Please check if your bounding box values latitude and longitude are within the project region</p>
+        </div>
+        """
+
 
 
 # ----------------------------------------------------------------
 #                               MAIN
 # ----------------------------------------------------------------
 
-bounding_box = {'lat': np.array([39.6, 42.7]),
-                'lon': np.array([19.3, 21.0])}
+# bounding_box = {'lat': np.array([39.6, 42.7]),
+#                 'lon': np.array([19.3, 21.0])}
 
-pdf_init = Cmst(bounding_box, sel_time_mean="annual",sel_experiment = 'rcp26', work_dir='../data')
+#pdf_init = Cmst(bounding_box, sel_time_mean="annual",sel_experiment = 'rcp26', work_dir='../data')
 
 hover = bmo.HoverTool(
     tooltips=[
@@ -92,37 +123,19 @@ TOOLS = "pan,wheel_zoom,box_zoom,reset,box_select,lasso_select, save"
 
 columns = [
     bmo.widgets.TableColumn(field="index", title="model"),
+    bmo.widgets.TableColumn(field="pr_percentiles", title="pr Percentile", width=70),
+    bmo.widgets.TableColumn(field="tasmin_percentiles", title="Tmin Perc.", width=65),
+    bmo.widgets.TableColumn(field="tasmax_percentiles", title="Tmax Perc.", width=65),
+    bmo.widgets.TableColumn(field="rsds_percentiles", title="Rsds Perc.", width=65),
 #    bmo.widgets.TableColumn(field="tasmax", title="tasmax"),
 #    bmo.widgets.TableColumn(field="pr", title="pr"),
 ]
 
-# Panel 1
+desc = bmo.Div(text=open(join(dirname(__file__), "description.html")).read(), width=800)
+
+# Create empty panel with empty tabs
 ls_tab = []
 pdf_ts = {}
-for k in pdf_init.time_selector:
-    pdf_ts[k] = bmo.ColumnDataSource(data=pdf_init.get_pandas_df(k))
-
-    # Create plot
-    p = bpl.figure(tools=[TOOLS,hover],toolbar_location="left")
-    p.circle(x='tasmax', y='pr',
-          source = pdf_ts[k],
-          size=12)
-
-    # Horizontal line
-    hline = bmo.Span(location=0, dimension='width', line_color='black', line_width=3)
-    p.renderers.extend([hline])
-
-    # Create table
-    data_table = bmo.widgets.DataTable(source=pdf_ts[k], columns=columns, fit_columns=True,
-                                       selectable='checkbox', width=500, height=500)
-
-    l = bo.layouts.layout([
-        bo.layouts.row([p, data_table]),
-        #bo.layouts.row([data_table])
-    ])
-
-    ls_tab.append(bmo.widgets.Panel(child=l, title=k))
-
 
 tabs = bmo.widgets.Tabs(tabs=ls_tab)
 
@@ -132,29 +145,29 @@ inp_lat = bmo.widgets.TextInput(title="Latitude (Format: [MIN, MAX])", value = "
 inp_lon = bmo.widgets.TextInput(title="Longitude (Format: [MIN, MAX])", value = "[19.3, 21.0]" )
 inp_time_mean = bmo.widgets.Select(title="Seasonal/Annual Mean:", value="annual", options=["annual", "summer", "winter"])
 inp_exp = bmo.widgets.Select(title="Experiment:", value="rcp26", options=["rcp26", "rcp45", "rcp85"])
-inp_update = bmo.widgets.Button(label="Update", button_type="success")
+inp_gen_upd= bmo.widgets.Button(label="Create Visualization", button_type="success")
 #x_axis = bmo.widgets.Select(title="X Axis", options=sorted(axis_map.keys()), value="Tomato Meter")
 #y_axis = bmo.widgets.Select(title="Y Axis", options=sorted(axis_map.keys()), value="Number of Reviews")
 
-inp_update.on_event(bo.events.ButtonClick, update_plot)
-inp_update.js_on_event(bo.events.ButtonClick, bmo.CustomJS(args=dict(div_spinner=div_spinner, spinner_text=spinner_text), code=show_spinner_js))
+# Handle on click_events (unfortunately show spinner with js due to lag otherwise)
+inp_gen_upd.on_event(bo.events.ButtonClick, gen_upd_plot)
+inp_gen_upd.js_on_event(bo.events.ButtonClick, bmo.CustomJS(args=dict(div_spinner=div_spinner, spinner_text=spinner_text), code=show_spinner_js))
 
-controls = [inp_lat, inp_lon, inp_time_mean, inp_exp, inp_update, div_spinner]
-
-inputs = bo.layouts.layout([
-bo.layouts.column([
+inputs = bo.layouts.row([
+    bo.layouts.column([
         bo.layouts.row([inp_lat, inp_lon]),
         bo.layouts.row([inp_time_mean, inp_exp]),
-        bo.layouts.row([inp_update])
-]),
-bo.layouts.column([
+        bo.layouts.row([inp_gen_upd])
+    ]),
+    bo.layouts.column([
         bo.layouts.row([div_spinner]),
-]),
+    ]),
 ])
 
 l = bo.layouts.layout([
-    bo.layouts.row([inputs]),
+    bo.layouts.row([desc]),
+    inputs,
     bo.layouts.row([tabs]),
-], sizing_mode='fixed')
+], sizing_mode='scale_width')
 
 bpl.curdoc().add_root(l)
